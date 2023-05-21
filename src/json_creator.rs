e079a8d2c6fa99da96eea1ea97df68705f8741f5
zuygui <maxime@halbitte.fr>
2023-05-21 13:37:44 +0200

    feat: :sparkles: write json transpiler

---
+++ src/json_creator.rs
@@ -0,0 +1,32 @@
+use std::io::Write;
+
+use crate::curse_api::CurseMod;
+
+
+/**
+ * 
+ *  Structure of the output JSON file
+ * 
+ * {
+ *   "curseFiles": [
+ *    {
+ *     "projectId": 49084,
+ *     "fileId": 981992
+ *    },
+ *  ]
+ * }
+ */
+
+pub fn compile_mods_to_json(mod_list: Vec<CurseMod>) {
+    let mut json = serde_json::json!({
+        "curseFiles": mod_list.iter().map(|mod_| {
+                        serde_json::json!({
+                            "projectId": mod_.mod_id,
+                            "fileId": mod_.file_id
+                        })
+                    }).collect::<Vec<serde_json::Value>>()
+    });
+
+    let mut file = std::fs::File::create("mods_list.json").unwrap();
+    file.write_all(json.to_string().as_bytes()).unwrap();
+}
